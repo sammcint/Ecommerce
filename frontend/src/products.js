3a03ea4bdc19cb80fcc3d1e0bdc1f8b68ad267bf
sammcint <sam.mcintyre26@gmail.com>
2021-03-14 14:10:05 -0400

    product details screen

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -7,7 +7,7 @@ const products = [
         'brand': 'FaZe',
         'category': 'Hoodies',
         'price': 59.99,
-        'countInStock': 10,
+        'countInStock': 0,
         'rating': 4.5,
         'numReviews': 12,
     },
