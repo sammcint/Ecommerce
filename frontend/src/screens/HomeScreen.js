95483399271dad645d89299aa7c7c8fefd14898f
sammcint <sam.mcintyre26@gmail.com>
2021-03-17 15:33:13 -0400

    user login reducer and action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -29,7 +29,7 @@ function HomeScreen() {
                     :  
                     <Row>
                         {products.map(product => (
-                            <Col key={product._id} sm={12} md={6} Lg={4} xL={3}>
+                            <Col key={product._id} sm={12} md={6} Lg={2} xL={3}>
                                 <Product product={product} />
                             </Col>
                         ))}
