b59bdfffc7f5a83879637dcc904d3a4fdd7b5fff
sammcint <sam.mcintyre26@gmail.com>
2021-03-18 16:51:08 -0400

    update profile endpoint

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -47,6 +47,24 @@ def registerUser(request):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['username']
+    user.email = data['email']
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
