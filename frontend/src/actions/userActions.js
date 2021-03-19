fcd3e33f1a90dc30c9f2088347501539970a8c59
sammcint <sam.mcintyre26@gmail.com>
2021-03-19 11:00:38 -0400

    log out bug fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -60,8 +60,8 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
-    dispatch({ type:USER_LOGOUT })
-    dispatch({ tpye: USER_DETAILS_RESET })
+    dispatch({ type: USER_LOGOUT })
+    dispatch({ type: USER_DETAILS_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
