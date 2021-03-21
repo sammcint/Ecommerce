119c103ea17f7ecbaf52e6323d0dd727e771a27b
sammcint <sam.mcintyre26@gmail.com>
2021-03-21 11:28:08 -0400

    backend order view & url

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -1,11 +1,11 @@
 from django.urls import path 
-#from base.views import order_views as views
+from base.views import order_views as views
 
 
 
 
 urlpatterns = [
 
-
+    path('add/', views.addOrderItems, name='orders-add'),
 
 ]
